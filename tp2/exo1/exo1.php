6d6ccc6d5a655622482b29d1275922b93b7f35a3
Tiger <129308718+lilrawry@users.noreply.github.com>
2024-11-11 09:36:34 +0100

    exo1.php

--- tp2/exo1/exo1.php
+++ tp2/exo1/exo1.php
@@ -3,42 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        form {
+            margin-bottom: 20px;
+        }
+        .result {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
 
     <?php
-        echo "Request received method :". $_SERVER["REQUEST_METHOD"];
+        echo "Request received method: " . htmlspecialchars($_SERVER["REQUEST_METHOD"]);
     ?>
     
     <hr>
 
-    <form action="form.php" method="post">
-        <label for="search">Search : </label>
-        <input type="text" name="search">
+    <form action="exo1.php" method="post">
+        <label for="search">Search: </label>
+        <input type="text" name="search" id="search" required>
         <br><br>
-
         <input type="submit" value="Envoyer">
     </form>
  
     <?php
 
-    $pers=array(
-        "pseudo_1"=>array("nom"=>"mossab_1","age"=>16),
-        "pseudo_2"=>array("nom"=>"mossab_2","age"=>16),
-        "pseudo_3"=>array("nom"=>"mossab_3","age"=>16),
+    $pers = array(
+        "pseudo_1" => array("nom" => "mossab_1", "age" => 16),
+        "pseudo_2" => array("nom" => "mossab_2", "age" => 16),
+        "pseudo_3" => array("nom" => "mossab_3", "age" => 16),
     );
 
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        if(isset($_POST["search"])){
-            if(array_key_exists($_POST["search"],$pers)){
-                echo "<hr>- nom :".$pers[$_POST["search"]]["nom"].
-                    "<br>- age :".$pers[$_POST["search"]]["age"];
-            }
-            else{
-                echo "<hr>user does not exists!";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST["search"])) {
+            $search = htmlspecialchars($_POST["search"]);
+            if (array_key_exists($search, $pers)) {
+                echo "<div class='result'><hr>- nom: " . htmlspecialchars($pers[$search]["nom"]) .
+                    "<br>- age: " . htmlspecialchars($pers[$search]["age"]) . "</div>";
+            } else {
+                echo "<div class='result'><hr>User does not exist!</div>";
             }
         }
-        
     }
     
     ?>
